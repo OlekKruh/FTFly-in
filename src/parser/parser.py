414e4615addb239239a074fa1b2b8008450a6d53
okruhlia <ksalexks@gmail.com>
2026-05-12 11:27:44 +0200

    test

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -56,6 +56,7 @@ class FileParser:
         elif line_list[0] in ["start_hub", "hub", "end_hub"]:
             try:
                 main_param = line_list[1]
+                meta_data = line_list[2] if len(line_list) > 2 else None
                 if len(main_param) != 3:
                     error_exit(f"Error: {line_list[0]} expects [name, x, y],"
                                f" got {main_param}")
@@ -66,7 +67,7 @@ class FileParser:
                 )
                 self.world.add_zone_to_map(
                     [hub_name, hub_x, hub_y],
-                    line_list[2] # выпадаем за список нужно условие
+                    meta_data # выпадаем за список нужно условие
                 )
             except (ValueError, IndexError) as e:
                 error_exit(f"Error: Invalid main parameters format "
@@ -75,7 +76,8 @@ class FileParser:
         elif line_list[0] == "connection":
             try:
                 connection = line_list[1]
-                meta_data = line_list[2] # выпадаем за список нужно условие
+                # выпадаем за список нужно условие
+                meta_data = line_list[2] if len(line_list) > 2 else None
                 if len(connection) != 1:
                     error_exit("Invalid value. "
                                "Onli one connection must be in line "
